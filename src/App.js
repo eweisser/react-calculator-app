cf8061684c0332be52060375aad1e831dfe124a7
Eric Weisser <ericwweisser@gmail.com>
2023-09-25 13:21:12 -0500

    improved layout for mobile views

--- src/App.js
+++ src/App.js
@@ -213,22 +213,6 @@ function App() {
 
       </div>
 
-
-
-      <header className="App-header">     {/* background is green */}
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
